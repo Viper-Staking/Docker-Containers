df46c9eacd9d76d0f93b6e85f5f8ebd0322bc993
Dylan Crocker <dcbn8@mst.edu>
2020-06-25 14:16:58 -0600

    Added start of a Jormungandr container.

---
+++ jormungandr/build.sh
@@ -0,0 +1,3 @@
+#docker login registry.gitlab.com
+docker build -t registry.gitlab.com/viper-staking/docker-containers/jormungandr:latest .
+#docker push registry.gitlab.com/viper-staking/docker-containers/jormungandr:latest
