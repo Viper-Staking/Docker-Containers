5c4859d472d78e00a430c8d039a357c9305457c7
Dylan Crocker <dcbn8@mst.edu>
2020-06-24 19:52:11 -0600

    Added initial cardano node Docker file and a build script for the base image.

---
+++ cardano-node/build.sh
@@ -0,0 +1,3 @@
+docker login registry.gitlab.com
+docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest .
+#docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest
