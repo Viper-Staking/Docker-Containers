e19569d138b10e908669e67231c688b5a0724bd7
Willie Marchetto <wrmarchetto@gmail.com>
2020-06-28 19:54:48 -0600

    Update terminal setup. Add gitlab-ci

--- debian-base/build.sh
+++ debian-base/build.sh
@@ -1,3 +1,4 @@
 docker login registry.gitlab.com
 docker build -t registry.gitlab.com/viper-staking/docker-containers/debian-base:latest .
-docker push registry.gitlab.com/viper-staking/docker-containers/debian-base:latest
+#docker push registry.gitlab.com/viper-staking/docker-containers/debian-base:latest
+docker run -it registry.gitlab.com/viper-staking/docker-containers/debian-base /usr/bin/zsh
