4031871ee3ae6e078e6dcc22f2e473b73eb38614
Willie Marchetto <wrmarchetto@gmail.com>
2020-07-17 19:15:41 -0600

    Refactor cardano-node dockerfile and run script

--- cardano-node/README.md
+++ cardano-node/README.md
@@ -10,14 +10,14 @@ which runs a relay node with the following parameters:
 
 The following command will run the container in this configuration:
 ``` bash
-docker run cardano-node
+docker run registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest
 ```
 
 ## Alternate configuration (block producing node)
 Below is an example on overriding the default CMD arguments to run cardano-node as a block
 producing node:
 ``` bash
-docker run cardano-node \
+docker run registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest \
     --database-path $HOME/cardano-node/db/ \
     --socket-path $HOME/cardano-node/db/node.socket \
     --host-addr "0.0.0.0" \
@@ -25,23 +25,46 @@ docker run cardano-node \
     --config $HOME/cardano-node/config.json \
     --topology $HOME/cardano-node/topology.json \
     --shelley-kes-key $HOME/cardano-node/pool_kes.skey \
-    --shelley-vrf-key $HOME/cardano-node/pool_vrf.skey \
+    --shelley-vrf-key $HOME/cardano-node/pool_vrf.skey
 ```
 
 ## Alternate configuration (relay node)
 Below is an example on overriding the default CMD arguments to run cardano-node as a relay
 node:
 ``` bash
-docker run cardano-node \
+docker run registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest \
     --database-path $HOME/cardano-node/db/ \
     --socket-path $HOME/cardano-node/db/node.socket \
     --port 4444 \
     --host-addr "0.0.0.0" \
-    --config $HOME/cardano-node/config.json \
+    --config $HOME/cardano-node/config.json
 ```
 
 ## Alternate configuration (shell entrypoint)
 Below is an example on overriding the entrypoint to enter the container in a shell:
 ``` bash
-docker run -it --entrypoint /usr/bin/zsh cardano-node
+docker run -it --entrypoint /usr/bin/zsh registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest
 ```
+
+## Running with run-cardano-node script
+`run-cardano-node.sh` is provided to simplify running cardano-node either via the docker 
+container or from a local installation. The first argument accepts a command prefix string
+which specifies where to execute cardano-node
+
+### Docker execution
+''' bash
+export CMD="docker run --mount type=bind,source=$PWD,target=/home/lovelace/cardano-node/ registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest"
+./run-cardano-node.sh $CMD
+'''
+
+### Local execution
+''' bash
+export CMD="/usr/local/bin/cardano-node"
+./run-cardano-node.sh $CMD
+'''
+
+## Required files
+* Node configuration [files](https://hydra.iohk.io/build/3462018/download/1/index.html)
+  * config.json
+  * topology.json
+  * shelley_testnet-shelley-genesis.json
--- cardano-node/run-cardano-node.sh
+++ cardano-node/run-cardano-node.sh
@@ -1,40 +1,66 @@
 #!/bin/bash
+# run-cardano-node
+# Arguments:
+#   1 - command prefix: Path to cardano-node executable or 'docker run' command [default: cardano-node]
+#   2 - base path: Path containing cardano-node directory
+#   3 - node mode: Node operation mode, "relay" or "pool" [default: relay]
+#   4 - node port [default: 4444]
+#   5 - host address [default: 0.0.0.0]
 
-if [ -z $1 ]; then
+if [ $# -ge 1 ]; then
+  CMD_PREFIX="$1"
+else
+  CMD_PREFIX="cardano-node"
+fi
+
+if [ $# -ge 2 ]; then
+  BASE_PATH=$2
+else
+  BASE_PATH=/home/lovelace
+fi
+
+if [ $# -ge 3 ]; then
+  CNODE_MODE=$3
+else
   CNODE_MODE="relay"
-else
-  CNODE_MODE=$1
 fi
 
-if [ -z $2 ]; then
+if [ $# -ge 4 ]; then
+  CNODE_PORT=$4
+else
   CNODE_PORT=4444
-else
-  CNODE_PORT=$2
 fi
 
-if [ -z $3 ]; then
-  HOST_ADDR="0.0.0.0"
+if [ $# -ge 5 ]; then
+  HOST_ADDR=$5
 else
-  HOST_ADDR=$3
+  HOST_ADDR="0.0.0.0"
 fi
+
+echo "Running cardano-node with the following parameters:"
+echo "  CMD_PREFIX = $CMD_PREFIX"
+echo "  BASE_PATH  = $BASE_PATH"
+echo "  CNODE_MODE = $CNODE_MODE"
+echo "  CNODE_PORT = $CNODE_PORT"
+echo "  HOST_ADDR  = $HOST_ADDR"
 
 if [ "$CNODE_MODE" = "relay" ]; then
-  cardano-node run \
-    --database-path $HOME/cardano-node/db/ \
-    --socket-path $HOME/cardano-node/db/node.socket \
+  eval $CMD_PREFIX run \
+    --database-path $BASE_PATH/cardano-node/db/ \
+    --socket-path $BASE_PATH/cardano-node/db/node.socket \
     --port $CNODE_PORT \
     --host-addr $HOST_ADDR \
-    --config $HOME/cardano-node/config.json \
-    --topology $HOME/cardano-node/topology.json
+    --config $BASE_PATH/cardano-node/config.json \
+    --topology $BASE_PATH/cardano-node/topology.json
 elif [ "$CNODE_MODE" = "pool" ]; then
-  cardano-node run \
-    --database-path $HOME/cardano-node/db/ \
-    --socket-path $HOME/cardano-node/db/node.socket \
+  eval $CMD_PREFIX run \
+    --database-path $BASE_PATH/cardano-node/db/ \
+    --socket-path $BASE_PATH/cardano-node/db/node.socket \
     --host-addr $HOST_ADDR \
     --port $CNODE_PORT \
-    --config $HOME/cardano-node/config.json \
-    --topology $HOME/cardano-node/topology.json \
-    --shelley-kes-key $HOME/cardano-node/pool_kes.skey \
-    --shelley-vrf-key $HOME/cardano-node/pool_vrf.skey \
-    --shelley-operational-certificate  $HOME/cardano-node/pool.cert
+    --config $BASE_PATH/cardano-node/config.json \
+    --topology $BASE_PATH/cardano-node/topology.json \
+    --shelley-kes-key $BASE_PATH/cardano-node/pool_kes.skey \
+    --shelley-vrf-key $BASE_PATH/cardano-node/pool_vrf.skey \
+    --shelley-operational-certificate $BASE_PATH/cardano-node/pool.cert
 fi
