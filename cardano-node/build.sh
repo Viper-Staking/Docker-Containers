e2f7194e4f187d60158280b4baddc1b720dfa47c
Dylan Crocker <dcbn8@mst.edu>
2020-07-25 11:58:38 -0600

    updated build script for specifying tag.

--- cardano-node/build.sh
+++ cardano-node/build.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ]; then
+    TAG="latest"
+else
+    TAG=$1
+fi
 docker login registry.gitlab.com
-docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest .
-docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:latest
+docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG .
+docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG
