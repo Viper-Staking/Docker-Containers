099330c2199d98400685b675351f7087ef68b42d
Dylan Crocker <dcbn8@mst.edu>
2020-07-25 11:52:36 -0600

    Updating to latest build.

--- cardano-node/slim/build.sh
+++ cardano-node/slim/build.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ]; then
+    TAG="latest"
+else
+    TAG=$1
+fi
 docker login registry.gitlab.com
-docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:slim .
-docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:slim
+docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:${TAG}-slim .
+docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:${TAG}-slim
